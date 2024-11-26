ae3aa8d04c5ec2eeac98987f744b1f82e2d4b8a0
victor <victorwohlers935@gmail.com>
2024-11-26 20:43:20 -0300

    commite victor teste

--- src/main/java/com/example/SitemaPizzaria/ProdutosRepository.java
+++ src/main/java/com/example/SitemaPizzaria/ProdutosRepository.java
@@ -1,8 +1,17 @@
 package com.example.SitemaPizzaria;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+    @Query("select p from pessoas p where p.nome like CONCAT('%', :nome, '%')")
+    List<Produto> buscarPorNome(@Param("nome")String nome);
+
+
 }
